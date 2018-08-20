85cdb20a2f4ed10c34cb43d8b9243632e41b8bef
Cody Hoover <git@codyhoover.com>
2018-08-20 14:35:31 +0200

    Separate post and get requests to different functions so they can have different authorization requirements

--- scores/index.ts
+++ scores/index.ts
@@ -1,41 +1,10 @@
-import { Context, HttpRequest, HttpMethod } from 'azure-functions-ts-essentials';
-import { BeatsaberLeaderboardParser, BeatSaber } from 'beatsaber-leaderboard-parser';
-
-import {
-    handleGenericError
-} from '../shared/function-utilities';
+import { Context, HttpRequest } from 'azure-functions-ts-essentials';
 
 export async function run(context: Context, req: HttpRequest): Promise<void> {
-    if (req.method === HttpMethod.Post) {
-        const scores = req.body;
+    context.res = {
+        status: 200,
+        body: context.bindings.currentLeaderboardBlobInput,
+    };
 
-        try {
-            // Validate that we can parse the file
-            const scoresObject = typeof scores === 'string' ?
-                JSON.parse(scores) :
-                scores;
-            BeatsaberLeaderboardParser.ParseFile(scoresObject);
-
-            // If we can, store it as a blob
-            context.bindings.currentLeaderboardBlobOutput = scores;
-
-            context.res = {
-                status: 200,
-                body: '',
-            };
-
-            return Promise.resolve();
-        } catch (e) {
-            context.log.error(e);
-            handleGenericError(context, 'Unable to parse scores.');
-        }
-    } else {
-        context.res = {
-            status: 200,
-            body: context.bindings.currentLeaderboardBlobInput,
-        };
-
-        return Promise.resolve();
-    }
-
+    return Promise.resolve();
 };
